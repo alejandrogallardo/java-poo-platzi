2714e0a9b313e8a8dc750b76a8356e02c0297a19
ridesnstuffgt@gmail.com <alejandrogallardo>
2022-01-21 14:49:13 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Doctor myDoctor = new Doctor("Fulanito");
+        myDoctor.name = "Alejandro Gallardo";
+        myDoctor.showName();
+    }
+}
