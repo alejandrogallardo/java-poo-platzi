5e107766e442b2840363c2c9357bc7b9092a952b
ridesnstuffgt@gmail.com <alejandrogallardo>
2022-01-22 15:24:17 -0600

    Creando elementos estáticos

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+// https://github.com/anncode1/curso-de-java-se-poo
+
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Fulanito");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Gallardo";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAle = new Doctor();
+        myDoctorAle.showId();
+
+        showMenu();
+
     }
+
 }
