812a31c80992d5d43b379fe4059dc8b1aa6917c3
ridesnstuffgt@gmail.com <alejandrogallardo>
2022-01-25 09:35:21 -0600

    clases anidadas

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 // https://github.com/anncode1/curso-de-java-se-poo
-
+import java.util.Date;
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+
+        /*Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Gallardo";
         myDoctor.showName();
         myDoctor.showId();
@@ -12,7 +13,34 @@ public class Main {
         Doctor myDoctorAle = new Doctor();
         myDoctorAle.showId();
 
-        showMenu();
+        showMenu();*/
+
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
+
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
+
+
+
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        Patient patient2 = new Patient("Anahi", "anahi@mail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
 
     }
 
